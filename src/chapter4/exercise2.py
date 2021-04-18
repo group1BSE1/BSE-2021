3d983cbf9fa4d62e8cf7ba77f09da4d155e36624
dead <dead@localhost.localdomain>
2021-04-18 08:24:49 +0300

    Assignment for chapter 3

--- src/chapter4/exercise2.py
+++ src/chapter4/exercise2.py
@@ -1,11 +1,14 @@
 def investment():
-    c = float(input("Enter C: "))
-    t = float(input("Enter time(t): "))
-    r = float(input("Enter rate(r): "))
-    n = float(input("Enter n: "))
-    a = (1 + (r/n))
-    b = t * n
-    d = a ** b
-    p = c * d
-    print('Final value',"%.2f"%(p))
+    try:
+        c = float(input("Enter C: "))
+        t = float(input("Enter time(t): "))
+        r = float(input("Enter rate(r): "))
+        n = float(input("Enter n: "))
+        a = (1 + (r/n))
+        b = t * n
+        d = a ** b
+        p = c * d
+        print('Final value',"%.2f"%(p))
+    except:
+        print('Please enter numerical input')
 investment()
--- src/chapter4/exercise6.py
+++ src/chapter4/exercise6.py
@@ -1,16 +1,21 @@
+hour = input('Enter hours: ')
+rat = input("Enter rate: ")
 def computepay():
-    hours = int(input('Enter hours: '))
-    rate = int(input("Enter rate: "))
-    pay = rate * hours
-    if hours>40:
+    try:
+        hours = int(hour)
+        rate = int(rat)
+        pay = rate * hours
+        if hours>40:
     #extra time
-        nhours = hours - 40
+            nhours = hours - 40
     #etra pay
-        npay = nhours*(10*1.5)
+            npay = nhours*(10*1.5)
     #pay for initial 40 hours 
-        nnpay = 40 * rate
+            nnpay = 40 * rate
     #new pay
-        newpay = nnpay + npay
-        print(newpay)
-    else: print(pay)
+            newpay = nnpay + npay
+            print(newpay)
+        else: print(pay)
+    except:
+        print('Error,Please enter numeric input')
 computepay()
