2e6dda0614ff9102308a03ef79a75feaf7fb82db
github_busingyeanthony <busingyeanthony2@gmail.com>
2021-05-06 11:27:34 +0300

    project file

---
+++ src/mid_sem/project.py
@@ -0,0 +1,40 @@
+while True:
+    try:
+        code = input("Enter customer code: ")
+        if code == 'r' or code=='R' or code =='c' or code=='C' or code =='i' or code == 'I':
+            tcode = 'ok'
+        else:
+            break
+        iread = float(input("Enter init read: "))
+        fread = float(input('Enter final reading: '))
+    except:
+        #print('Please enter valid input')
+        break
+    
+    if iread>0 and fread<999999999:
+        if iread<fread:
+            gallons = (fread-iread)/10
+        else:
+            gallons = ((1000000000-iread)+fread)/10
+        #print(gallons)
+    if code =='r' or code == 'R':
+        amount = 5 + (0.0005*gallons)
+    elif code =="c" or code == 'C':
+        if gallons <= 4000000:
+            amount = 1000
+        else:
+            ngallons = gallons - 4000000
+            amount = 1000 + (0.00025 *ngallons)
+    else:
+        if code =='i' or code =='I':
+            if gallons<=4000000:
+                amount =  1000
+            elif gallons >4000000 and gallons < 10000000:
+                ngallons = gallons - 10000000
+                amount = 2000 + (0.00025*ngallons)
+    
+    print('Customer code:', code)
+    print('Initial reading: ',iread)
+    print('Final reading: ',fread)
+    print('Gallons used: ',gallons)
+    print('Amount used: ', round(amount,2))
